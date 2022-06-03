93a3f29551d4542656501605687d794d4c82cff9
Thiago Augusto <thiago.digitalfront@gmail.com>
2022-06-03 15:19:42 -0300

    update shell script to set 'service_name' field

--- run.sh
+++ run.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
-echo '==> Create lumen project ***'
-docker run --rm --interactive --tty -v $PWD/micro01:/app composer create-project --prefer-dist laravel/lumen ./
+if [ -f .env ]; then
+    export $(cat .env | xargs)
 
-echo '==> Uploading Application container ***' 
-docker-compose up --build -d
+    if [ $SERVICE_NAME == '']; then
+        echo '"SERVICE_NAME" field not found in .env file'
+    else
+        echo '==> Create lumen project ***'
+        docker run --rm --interactive --tty -v $PWD/${SERVICE_NAME}:/app composer create-project --prefer-dist laravel/lumen ./
 
-echo '==> Install dependencies ***'
-docker run --rm --interactive --tty -v $PWD/micro01:/app composer install
+        echo '==> Uploading Application container ***' 
+        docker-compose up --build -d
 
-echo '==> Make migrations ***'
-docker exec -it php php /var/www/html/artisan migrate
+        echo '==> Install dependencies ***'
+        docker run --rm --interactive --tty -v $PWD/${SERVICE_NAME}:/app composer install
 
-echo '==> Information of new containers ***'
-docker ps -a 
+        echo '==> Make migrations ***'
+        docker exec -it php php /var/www/html/artisan migrate
+
+        echo '==> Information of new containers ***'
+        docker ps -a 
+    fi
+else
+    echo '.env file not found'
+fi
