820c07fef6505173da7eb4552fd48e74be8340f1
Thiago Augusto <thiago.digitalfront@gmail.com>
2022-06-04 14:58:05 -0300

    create develop repository

--- run.sh
+++ run.sh
@@ -2,17 +2,21 @@
 if [ -f .env ]; then
     export $(cat .env | xargs)
 
-    if [ $SERVICE_NAME == '']; then
-        echo '"SERVICE_NAME" field not found in .env file'
+    if [ $AUTH_USER == '']; then
+        echo '"AUTH_USER" field not found in .env file'
     else
         echo '==> Create lumen project ***'
-        docker run --rm --interactive --tty -v $PWD/${SERVICE_NAME}:/app composer create-project --prefer-dist laravel/lumen ./
+        docker run --rm --interactive --tty -v $PWD/${AUTH_PATH}:/app composer create-project --prefer-dist laravel/lumen ./
 
         echo '==> Uploading Application container ***' 
         docker-compose up --build -d
 
         echo '==> Install dependencies ***'
-        docker run --rm --interactive --tty -v $PWD/${SERVICE_NAME}:/app composer install
+        docker run --rm --interactive --tty -v $PWD/${AUTH_PATH}:/app composer install
+        docker run --rm --interactive --tty -v $PWD/${AUTH_PATH}:/app chown  ${AUTH_USER}:${AUTH_USER} auth-service/*
+
+        echo '==> Install Packages ***'
+        docker run --rm --interactive --tty -v $PWD/${AUTH_PATH}:/app composer require wn/lumen-generators
 
         echo '==> Make migrations ***'
         docker exec -it php php /var/www/html/artisan migrate
