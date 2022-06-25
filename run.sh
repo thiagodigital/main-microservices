6c16e2ba6ab975c4c3786debdcd1f0b33e34ec25
Thiago Augusto <thiago.digitalfront@gmail.com>
2022-06-25 19:09:25 -0300

    simple api laravel

--- run.sh
+++ run.sh
@@ -1,25 +1,18 @@
 #!/bin/bash
-if [ -f .env ]; then
-    export $(cat .env | xargs)
+echo '==> Create lumen project ***'
+docker run --rm --interactive --tty -v $PWD/api-service:/app composer create-project --prefer-dist laravel/laravel ./
 
-    if [ $SERVICE_NAME == '']; then
-        echo '"SERVICE_NAME" field not found in .env file'
-    else
-        echo '==> Create lumen project ***'
-        docker run --rm --interactive --tty -v $PWD/${SERVICE_NAME}:/app composer create-project --prefer-dist laravel/lumen ./
+echo '==> Uploading Application container ***'
+docker-compose up --build -d
 
-        echo '==> Uploading Application container ***' 
-        docker-compose up --build -d
+echo '==> Install dependencies ***'
+docker run --rm --interactive --tty -v $PWD/api-service:/app composer install
 
-        echo '==> Install dependencies ***'
-        docker run --rm --interactive --tty -v $PWD/${SERVICE_NAME}:/app composer install
+echo '==> Set user folders and group ***'
+docker run --rm --interactive --tty -v $PWD/api-service:/app chown thiago:thiago -R .
 
-        echo '==> Make migrations ***'
-        docker exec -it php php /var/www/html/artisan migrate
+echo '==> Make migrations ***'
+docker exec -it php php /var/www/html/artisan migrate
 
-        echo '==> Information of new containers ***'
-        docker ps -a 
-    fi
-else
-    echo '.env file not found'
-fi
+echo '==> Information of new containers ***'
+docker ps -a
